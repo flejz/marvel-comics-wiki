426469b28deca611f0dad3b779a8db40f9740191
Jaime Flores <jaimelopesflores@gmail.com>
2016-10-05 18:05:40 -0300

    Step 1

--- app/src/main/java/com/example/gdg/marvel/view/events/CharactersEvents.java
+++
@@ -1,8 +0,0 @@
-package com.example.gdg.marvel.view.events;
-
-import com.karumi.marvelapiclient.model.CharacterDto;
-
-public interface CharactersEvents {
-
-    void onSelect(CharacterDto character);
-}
